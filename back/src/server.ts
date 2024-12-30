4bdf32d8e5a60b5602bdb64dd4dea688b812e650
Eduardo Cárdenas <jcardenasya@gmail.com>
2024-12-29 19:14:43 -0600

    React LifeCycle

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import indexRouter from "./routes/indexRouter";
+import cors from "cors";
 
 const server = express();
 
 //Middleware de express
 server.use(express.json());
+server.use(cors());
 server.use(indexRouter);
 
 export default server;
